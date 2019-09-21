f27c1a0bb4a6c7a5f28e898cca077c64c12d7d3d
Your Name <you@example.com>
2019-09-21 14:22:57 +0000

    hello World!! by Vue.js

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,26 +1,37 @@
 <html>
 <head>
-  <title>RailsTutorial</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>RailsTutorial</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Vueのインストールを行う -->
+    <script src="https://unpkg.com/vue@2.5.17"></script>
 </head>
 <!-- テンプレート用 -->
 <body>
-  <!-- ４　npm run devでソースをコンパイルする
-       ５  view内でasset('/js/()')でassetを読み込む -->
-  <script src="{{ asset('/js/samplevue.js') }}"></script>
-  <div class="container">
-    <header>
-      @yield('header')
-    </header>
-    <main>
-      @yield('main')
-    </main>
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-    <footer>
-      @yield('footer')
-    </footer>
-  </div>
+    <!-- Vueによってレンダリングされる部分です
+         script以下のコードがdivの中に反映されます -->
+    <div id="app"></div>
+    <!--  script以下はVueを実行するためのコードです
+          bladeの{{$変数}}記法とコンフリクトするので、{{}}で囲うvueの式を描く場合は
+　　　　　 {{}}の前に＠をつけましょう -->
+    <script>
+        new Vue({
+            template: '<p>@{{msg}}</p>',
+            data: { msg: 'hello world!' }
+        }).$mount('#app')
+    </script>
+    <div class="container">
+        <header>
+            @yield('header')
+        </header>
+        <main>
+            @yield('main')
+        </main>
+        <aside class="sidebar">
+            @yield('sidebar')
+        </aside>
+        <footer>
+            @yield('footer')
+        </footer>
+    </div>
 </body>
 </html>
